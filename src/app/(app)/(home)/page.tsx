05a5fa3f0d23044506336a382d6d5bea6c6ad069
Abraar Ahmed <abraar@rampinteractive.com>
2025-05-05 14:50:00 -0600

    payload integration

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,27 +1,17 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Progress } from "@/components/ui/progress";
-import { Textarea } from "@/components/ui/textarea";
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
 
-export default function Home() {
+export default async function Home() {
+  const payload = await getPayload({
+      config: configPromise,
+    })
+  
+    const data = await payload.find({
+      collection: 'categories',
+    })
   return (  
-    <div className="p-4">
-    <div className="flex flex-col gap-y-4">
-      <div>
-        <Button>
-          I am a button
-        </Button>
-      </div>
-      <div>
-        <Input placeholder="I am an input" />
-      </div>
-      <div>
-        <Progress value={50}/>
-      </div>
-      <div>
-        <Textarea value="I am a text area"></Textarea>
-      </div>
-    </div>
+    <div>
+      {JSON.stringify(data, null, 2)}
     </div>
   ); 
 }
