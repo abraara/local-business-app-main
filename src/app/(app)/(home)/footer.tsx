4bd52b70d8c60b7a123178dbaecd47ae60f6e6c8
Abraar Ahmed <abraar@rampinteractive.com>
2025-05-05 01:03:35 -0600

    payload commit

---
+++ src/app/(app)/(home)/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <footer className="flex border-4 justify-between font-medium p-4">
+            <div className="flex items-center gap-2">
+                <p>&copy; 2023 Your Company. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+}
---
+++ src/app/(app)/(home)/page.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
+export default function Home() {
+  return (  
+    <div className="p-4">
+    <div className="flex flex-col gap-y-4">
+      <div>
+        <Button>
+          I am a button
+        </Button>
+      </div>
+      <div>
+        <Input placeholder="I am an input" />
+      </div>
+      <div>
+        <Progress value={50}/>
+      </div>
+      <div>
+        <Textarea value="I am a text area"></Textarea>
+      </div>
+    </div>
+    </div>
+  ); 
+}
