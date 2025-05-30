8b9ef9766c7f040e2660790cc9947b50cef02221
Abraar Ahmed <abraar@rampinteractive.com>
2025-05-30 11:32:15 -0600

    authentication

---
+++ src/app/(app)/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view"
+
+const Page = () => {
+    return (
+        <SignInView>
+            
+        </SignInView>
+    )
+}
+
+export default Page;
