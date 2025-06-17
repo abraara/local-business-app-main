690330939afe0f29a42500a6e06171e3c4301d40
Abraar Ahmed <abraar@rampinteractive.com>
2025-06-17 10:33:02 -0600

    image fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,28 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'shoppinlocal.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.shoppinlocal.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default withPayload(nextConfig);
