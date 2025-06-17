4627623f17798ec4fc96a30aed24c5d3654ef7f1
Abraar Ahmed <abraar@rampinteractive.com>
2025-06-16 21:08:48 -0600

    subdomain

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,8 +6,10 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateTenantUrl(tenantSlug: string) {
+  const isDevelopment = process.env.NODE_ENV === "development";
+  const isSubdomainRoutingEnabled = Boolean(process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING!);
 
-  if (process.env.NODE_ENV === "development") {
+  if (isDevelopment || !isSubdomainRoutingEnabled) {
     return `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${tenantSlug}`;
   }
 
